f86e3bacadbe1011374b2c37e91b10f0b4fb332b
OmarMWarraich <earthling986@gmail.com>
2024-02-08 14:07:31 +0500

    feat: redux installation, store, rootReducer

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.scss';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+
 import App from './App';
 import { UserProvider } from './contexts/user/user.context'
 import { CategoriesProvider } from './contexts/categories/categories.context';
 import { CartProvider } from './contexts/cart/cart.context';
+
+import './index.scss';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store = {store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
