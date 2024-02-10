a03fb9968d08f95d30f3f51999e807ec6e1c1e45
OmarMWarraich <earthling986@gmail.com>
2024-02-10 19:26:35 +0500

    feat: Payment Form, Serverless Function

---
+++ netlify/functions/create-payment-intent.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+exports.handler = async (e) => {
+    try {
+        const { amount } = JSON.parse(e.body);
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: "usd",
+            payment_method_types: ["card"]
+        });
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ paymentIntent })
+        }       
+    } catch (error) {
+
+        console.error(error);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: "Failed to create payment intent" })
+        }
+        
+    }
+}
